834244aa7eacef62a7770b51a9a96d53793e2fd8
Wendell Adriel <wendelladriel.ti@gmail.com>
2024-05-16 21:32:17 +0100

    Initial structure

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+    <h1>Simple DI Container</h1>
+</div>
+
+Simple DI container implementation example.
+
+## Credits
+
+- [Wendell Adriel](https://github.com/WendellAdriel)
---
+++ src/Container/Container.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WendellAdriel\SimpleContainer\Container;
+
+use Psr\Container\ContainerInterface;
+
+final readonly class Container implements ContainerInterface
+{
+    public function get(string $id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has(string $id): bool
+    {
+        // TODO: Implement has() method.
+    }
+}
